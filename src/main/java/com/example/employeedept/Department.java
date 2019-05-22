cda87cc37b46ec00a443db8056fd2b1c02ad165c
Cesar Mejia <crsmejia93@gmail.com>
2019-05-22 16:56:26 -0400

    Initial commit

---
+++ src/main/java/com/example/employeedept/Department.java
@@ -0,0 +1,56 @@
+package com.example.employeedept;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Set;
+
+@Entity
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    @Size(min = 5)
+    private String name;
+
+    @NotNull
+    @Size(min = 5)
+    private String company;
+
+    @OneToMany(mappedBy = "department", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    public Set<Employee> employees;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public Set<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(Set<Employee> employees) {
+        this.employees = employees;
+    }
+}
---
+++ src/main/java/com/example/employeedept/HomeController.java
@@ -0,0 +1,4 @@
+package com.example.employeedept;
+
+public class HomeController {
+}
